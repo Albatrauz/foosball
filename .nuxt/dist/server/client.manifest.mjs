c9c7bcca85824a945cf749d65e6ee91fc48c1ca1
Albatrauz <michaelvg94@gmail.com>
2024-09-16 22:31:05 +0200

    added nuxt

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/michael/workspace/foosball/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/michael/workspace/foosball/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
